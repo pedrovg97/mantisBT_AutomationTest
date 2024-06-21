d61f799752c31bb42eb837b2c114e9ccbc7fc3f7
pedrocarvalho <pedro.bento.162@ufrn.edu.br>
2024-06-21 18:04:12 -0300

    criacao de pastas

--- Testes/testeCriarTarefa.cs
+++ Testes/testeCriarTarefa.cs
@@ -1,10 +1,11 @@
-﻿using NUnit.Framework;
+﻿using mantisBT_AutomationTest.paginas;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace mantisBT_AutomationTest
+namespace mantisBT_AutomationTest.Testes
 {
-    public class PaginaCriarTarefa : PaginaLogin
+    public class TesteCriarTarefa : PaginaLogin
     {
         [Test]
         public void acessarPaginaCriarTarefa()
@@ -12,6 +13,12 @@ namespace mantisBT_AutomationTest
             var button1 = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(".btn.btn-primary.btn-sm")));
             button1.Click();
             Assert.IsTrue(driver.Url.Contains("https://mantis-prova.base2.com.br/bug_report_page.php"));
+        }
+
+        public void cadastrartarefaPreenchendo()
+        {
+
+
         }
 
 
