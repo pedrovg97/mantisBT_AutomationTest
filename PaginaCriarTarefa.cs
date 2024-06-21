47741af4444ae3346a03c52b28ffa0e63b2c0407
pedrocarvalho <pedro.bento.162@ufrn.edu.br>
2024-06-21 17:40:33 -0300

    Adicionar arquivos de projeto.

---
+++ PaginaCriarTarefa.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace mantisBT_AutomationTest
+{
+    public class PaginaCriarTarefa : PaginaLogin
+    {
+        [Test]
+        public void acessarPaginaCriarTarefa()
+        {
+            var button1 = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(".btn.btn-primary.btn-sm")));
+            button1.Click();
+            Assert.IsTrue(driver.Url.Contains("https://mantis-prova.base2.com.br/bug_report_page.php"));
+        }
+
+
+    }
+}
---
+++ PaginaLogin.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace mantisBT_AutomationTest
+{
+    public class PaginaLogin
+    {
+        public IWebDriver driver;
+        public WebDriverWait wait;
+
+        [SetUp]
+        public void carregarPaginaLogin()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Navigate().GoToUrl("https://mantis-prova.base2.com.br/login_page.php");
+            driver.Manage().Window.Maximize();
+            realizarLogin();
+        }
+
+        public void botaoEntrar()
+        {
+            var button = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("input[type='submit'][value='Entrar']")));
+            button.Click();
+        }
+        public void realizarLogin()
+        {
+            driver.FindElement(By.Id("username")).SendKeys("Pedro_Bento");
+            botaoEntrar();
+            driver.FindElement(By.Id("password")).SendKeys("pedroobnt");
+            botaoEntrar();
+        }
+    }
+}
